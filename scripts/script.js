3699e5f7134c4362ee052ea5f7124f6d2329419f
Laurabinky <91797246+Laurabinky@users.noreply.github.com>
2023-11-07 12:57:59 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,28 @@ console.log("Howdy!");
 
 // Voor een geluidje bij het klikken
 
-var audio = new Audio("audio/")
+var audio = new Audio("audio/toet.mp3")
+var stuur = document.querySelector("article section:nth-of-type(2) img:nth-of-type(1)");
 
+stuur.addEventListener("click", () => {
+    audio.play();
+    console.log("hallocc")
+});
+
+
+
+//  Navigeren tussen de items
 const pook = document.querySelector("article section img:nth-of-type(2)")
+
+// Dit vertelt mij alles wat ik wil weten van alle kanten van de img
 const pookInfo = pook.getBoundingClientRect()
+
 const fordImages = document.querySelectorAll("img")
 let initialX
 
 pook.addEventListener("drag", onDrag)
 
 function dragStart(event) {
-    // Dit vertelt mij alles wat ik wil weten van alle kanten van de img
     initialX = pookInfo.x
 
     // console.log(initialX)
