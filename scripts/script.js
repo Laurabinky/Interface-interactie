cbb23b9a59b1bd382351fad225efcd6dbcec2521
Laurabinky <91797246+Laurabinky@users.noreply.github.com>
2023-11-07 11:13:13 +0100

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,28 @@
 // JavaScript Document
 console.log("Howdy!");
+
+const pook = document.querySelector("article section img:nth-of-type(2)")
+const pookInfo = pook.getBoundingClientRect()
+const fordImages = document.querySelectorAll("img")
+let initialX
+
+pook.addEventListener("drag", onDrag)
+
+function dragStart(event) {
+    // Dit vertelt mij alles wat ik wil weten van alle kanten van de img
+    initialX = pookInfo.x
+
+    // console.log(initialX)
+}
+
+function onDrag(event) {
+    const currentX = event.clientX
+    const deltaX = currentX - initialX
+
+if (deltaX > 170) {
+    fordImages[0].src = ""
+    fordImages[1].SRC = ""
+} else {
+    return
+}
+}
