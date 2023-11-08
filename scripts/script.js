283075e984e2a0b62330ac477f3deec16e63e0b7
Laurabinky <91797246+Laurabinky@users.noreply.github.com>
2023-11-08 10:40:06 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -9,21 +9,32 @@ var stuur = document.querySelector("article section:nth-of-type(2) img:nth-of-ty
 
 stuur.addEventListener("click", () => {
     audio.play();
-    console.log("hallocc")
 });
 
 
 
+// Zichtbaar maken van het 1e stukje tekst
+
+var stuurKlik = document.querySelector("article section:nth-of-type(2) img:nth-of-type(1)")
+var liZichtbaar = document.querySelector("article section > ul li:first-of-type")
+
+stuurKlik.addEventListener("click", () => {
+    liZichtbaar.classList.add("zichtbaar")
+})
+
+
+
 //  Navigeren tussen de items
-const pook = document.querySelector("article section img:nth-of-type(2)")
+const pook = document.querySelector("article section img:nth-of-type(3)")
 
 // Dit vertelt mij alles wat ik wil weten van alle kanten van de img
 const pookInfo = pook.getBoundingClientRect()
-
-const fordImages = document.querySelectorAll("img")
+const timelineItems = document.querySelectorAll("article > section:nth-of-type(1) > ul > li")
+const radio = document.querySelector("article section:nth-of-type(2) img:nth-of-type(2)")
 let initialX
+let currentIndex = 0
 
-pook.addEventListener("drag", onDrag)
+console.log(timelineItems)
 
 function dragStart(event) {
     initialX = pookInfo.x
@@ -31,14 +42,44 @@ function dragStart(event) {
     // console.log(initialX)
 }
 
-function onDrag(event) {
+function showItem() {
+timelineItems.forEach((li, index) => {
+if (index == currentIndex) {
+    li.classList.remove("hidden")
+} else {
+    li.classList.add("hidden")
+}
+})
+}
+
+pook.addEventListener("drag", (event) => {
     const currentX = event.clientX
     const deltaX = currentX - initialX
 
-if (deltaX > 170) {
-    fordImages[0].src = ""
-    fordImages[1].SRC = ""
-} else {
+    // console.log(deltaX)
+
+if (deltaX > 170 && currentIndex == 2) {
+    // Met % geef je aan dat als het aantal groter is dan het aantal wat er is ga je terug naar item 1
+currentIndex = (currentIndex + 1) % timelineItems.length
+
+    radio.src = ""
+
+    showItem()
+} else if (deltaX > 170 && currentIndex == 5) {
+    currentIndex = (currentIndex + 1) % timelineItems.length
+
+    radio.src = ""
+
+    showItem()
+
+} else if (deltaX > 170) {
+    currentIndex = (currentIndex + 1) % timelineItems.length
+    showItem()
+
+}
+else {
     return
 }
-}
+})
+
+console.log(currentIndex)
