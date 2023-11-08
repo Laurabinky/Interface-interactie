83e90bbe508fe53806aa6f15e6ddfe5439fd5f3d
Laurabinky <91797246+Laurabinky@users.noreply.github.com>
2023-11-08 11:50:24 +0100

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -13,73 +13,109 @@ stuur.addEventListener("click", () => {
 
 
 
-// Zichtbaar maken van het 1e stukje tekst
-
-var stuurKlik = document.querySelector("article section:nth-of-type(2) img:nth-of-type(1)")
-var liZichtbaar = document.querySelector("article section > ul li:first-of-type")
-
-stuurKlik.addEventListener("click", () => {
-    liZichtbaar.classList.add("zichtbaar")
-})
-
-
-
 //  Navigeren tussen de items
+
 const pook = document.querySelector("article section img:nth-of-type(3)")
 
 // Dit vertelt mij alles wat ik wil weten van alle kanten van de img
 const pookInfo = pook.getBoundingClientRect()
 const timelineItems = document.querySelectorAll("article > section:nth-of-type(1) > ul > li")
 const radio = document.querySelector("article section:nth-of-type(2) img:nth-of-type(2)")
+
+// Deze moeten veranderen in de function
 let initialX
 let currentIndex = 0
+let eventOn = true
 
-console.log(timelineItems)
 
-function dragStart(event) {
-    initialX = pookInfo.x
-
-    // console.log(initialX)
-}
-
+// Zet een class met hidden op alle li'tjes
 function showItem() {
-timelineItems.forEach((li, index) => {
-if (index == currentIndex) {
-    li.classList.remove("hidden")
+    timelineItems.forEach((li, index) => {
+        if (index == currentIndex) {
+            li.classList.remove("hidden")
+        } else {
+            li.classList.add("hidden")
+        }
+    })
+}
+showItem()
+
+// Kijkt naar of er gesleept wordt en geeft de info aan de let
+function dragStart(event) {
+if (!eventOn) {
+    eventOn = true
+    initialX = pookInfo.x
+onDrag()
 } else {
-    li.classList.add("hidden")
-}
-})
+    initialX = pookInfo.x
+    }
 }
 
-pook.addEventListener("drag", (event) => {
-    const currentX = event.clientX
-    const deltaX = currentX - initialX
+pook.addEventListener("drag", function onDrag(event) {
+if (eventOn == false) {
+    return
+}
 
-    // console.log(deltaX)
+// Deze variabelen moeten hier anders wordt de code ontregeld
+const currentX = event.clientX
+const deltaX = currentX - initialX
 
 if (deltaX > 170 && currentIndex == 2) {
     // Met % geef je aan dat als het aantal groter is dan het aantal wat er is ga je terug naar item 1
-currentIndex = (currentIndex + 1) % timelineItems.length
-
-    radio.src = ""
-
-    showItem()
-} else if (deltaX > 170 && currentIndex == 5) {
     currentIndex = (currentIndex + 1) % timelineItems.length
-
+    // Veranderen van de radio
     radio.src = ""
 
+    // Zodat hij opnieuw gaat kijken naar de statement
     showItem()
+    eventOn = false
+
+    // Zodat je opnieuw de pook kan bewegen
+    setTimeout(() => {
+        eventOn = true
+    }, 10000)
+    
+} else if (deltaX > 170 && currentIndex == 5) {
+
+    currentIndex = (currentIndex + 1) % timelineItems.length
+    // Veranderen van de radio 2
+    radio.src = ""
+
+    // Zodat hij opnieuw gaat kijken naar de statement en de class op de juiste plek zet
+    showItem()
+    eventOn = false
+
+    // Zodat je opnieuw de pook kan bewegen
+    setTimeout(() => {
+        eventOn = true
+    }, 10000)
 
 } else if (deltaX > 170) {
+    // Zorgt ervoor dat je meerdere malen door de rij heen kan navigeren
     currentIndex = (currentIndex + 1) % timelineItems.length
+    
+    // Zodat hij opnieuw gaat kijken naar de statement
     showItem()
+    eventOn = false
 
+     // Zodat je opnieuw de pook kan bewegen
+    setTimeout(() => {
+        eventOn = true
+    }, 10000)
 }
 else {
     return
 }
 })
 
-console.log(currentIndex)
+
+
+// Zichtbaar maken van het 1e stukje tekst
+
+var stuurKlik = document.querySelector("article section:nth-of-type(2) img:nth-of-type(1)")
+
+stuurKlik.addEventListener("click", () => {
+    timelineItems[currentIndex].classList.add("zichtbaar")
+
+    console.log("test")
+})
